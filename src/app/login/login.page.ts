6abb25a249fd2b9d57901548fd5360a78a0881f2
Thaís Ohashi <ohashithais@gmail.com>
2024-06-23 17:12:28 -0300

    add home, login and register style (#1)

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['login.page.scss'],
 })
 export class LoginPage {
+  constructor(private router: Router) {}
 
-  constructor() {}
-
-  onFirstButtonClick() {}
-  onSecondButtonClick() {}
-
+  onHomeButtonClick() {
+    this.router.navigate(['/']).then(
+      (nav) => {
+        return;
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['register.page.scss'],
 })
 export class RegisterPage {
+  constructor(private router: Router) {}
 
-  constructor() {}
-
-  onFirstButtonClick() {}
-  onSecondButtonClick() {}
-
+  onHomeButtonClick() {
+    this.router.navigate(['/']).then(
+      (nav) => {
+        return;
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
 }
