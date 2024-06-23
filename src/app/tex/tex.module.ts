5c670c6f489aed8e3833195e5371b597259e2e2b
Isabella Ramos Carvalho <au.isabella.r.c@hotmail.com>
2024-06-23 14:42:44 -0300

    chore: menu were added to tex page

--- src/app/tex/tex.module.ts
+++ src/app/tex/tex.module.ts
@@ -5,15 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { TexPage } from './tex.page';
 
 import { TexPageRoutingModule } from './tex-routing.module';
+import { HeaderPageModule } from "../component/header/header.module";
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    TexPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        TexPageRoutingModule,
+        HeaderPageModule
+    ],
   declarations: [TexPage]
 })
 export class TexPageModule {}
