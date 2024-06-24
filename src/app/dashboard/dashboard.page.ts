b569daa217a6823fa36cdc854fefa85080d6b3d9
Isabella Ramos Carvalho <au.isabella.r.c@hotmail.com>
2024-06-24 06:39:01 -0300

    fix: dashboard adjustments

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -9,6 +9,14 @@ export class DashboardPage {
     siteTitle: string = 'COMO OS EXERCÍCIOS FÍSICOS AJUDAM NA SAÚDE MENTAL?';
     siteContent: string = 'Não é segredo para ninguém que a prática regular de exercícios físicos traz uma série de benefícios para o organismo. O háb...';
 
+    btns = [
+        { label: 'Contato' },
+        { label: 'Gadgets' },
+        { label: 'Registro' },
+        { label: 'Relatórios' },
+        { label: 'Configuração' },
+        { label: 'Logout' },
+    ];
   constructor () {}
 
 }
